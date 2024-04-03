23c1ce8c043514aecfa5017b1f9fc94b294a3066
Halid <halid.ali@ul.com>
2024-04-03 14:49:18 +0200

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: '4mwuse',
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
     specPattern: ['**/*.spec.ts', '**/*.cy.ts'],
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
