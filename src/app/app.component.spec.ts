99df3b6da7c2af9d51cd4a850c59ee32fef2aef3
Halid <halid.ali@ul.com>
2024-04-03 14:41:19 +0200

    create a form and write tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-    })
-  );
+      imports: [ReactiveFormsModule],
+    });
+    cy.viewport(700, 700);
+  });
 
-  it(`should have as title 'cypress-demo'`, () => {
+  it('mounts', () => {
     cy.mount(AppComponent);
-    cy.get('title').should('contain.text', 'cypress-demo');
+  });
+
+  describe('when first name', () => {
+    it('is empty, should display error', () => {
+      cy.mount(AppComponent);
+      cy.get('#firstName').type('Very very very very long first name').blur();
+      cy.get('#firstNameErrors').should(
+        'contain.text',
+        'At most 30 characters',
+      );
+    });
+
+    it('is too long, should display error', () => {
+      cy.mount(AppComponent);
+      cy.get('#firstName').focus().wait(1).blur();
+      cy.get('#firstNameErrors').should(
+        'contain.text',
+        'First Name is required',
+      );
+    });
+
+    it('fulfills criteria, should not display error', () => {
+      cy.mount(AppComponent);
+      cy.get('#firstName').type('Halid').blur();
+      cy.get('#firstNameErrors').should('not.exist');
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cypress-demo';
+  form: FormGroup;
+  isButtonDisabled = true;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-Z ]+$'),
+        ],
+      ],
+      lastName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-Z ]+$'),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ],
+      ],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]{4}-[0-9]{3}-[0-9]{4}$'),
+        ],
+      ],
+    });
+  }
+
+  getErrorMessage(errorName: string, controlName: string, param?: string) {
+    switch (errorName) {
+      case 'required':
+        return `${controlName} is required`;
+      case 'minLength':
+        return `At least ${param} characters`;
+      case 'maxLength':
+        return `At most ${param} characters`;
+      case 'pattern':
+        return `Invalid ${controlName}`;
+      default:
+        return 'Unknown error type';
+    }
+  }
+
+  isValid(name: string): boolean {
+    let control = this.form.controls[name];
+    return control.invalid && this.form.invalid && control.touched;
+  }
+
+  hasError(controlName: string, errorName: string) {
+    let control = this.form.controls[controlName];
+    return control.errors?.[errorName];
+  }
 }
